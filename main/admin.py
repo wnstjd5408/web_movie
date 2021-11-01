8766427dcfe6742e7f1823589f19252aa1dcd05f
wnstjd5408 <wnstjd5408@naver.com>
2021-11-01 13:28:38 +0900

    관리자 페이지 수정

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Theater, Auditorium
 # Register your models here.
 from django.utils.html import format_html
 
@@ -14,3 +14,15 @@ class MovieAdmin(admin.ModelAdmin):
     def image_tag(self, obj):
         return format_html('<img src="{}" width="50px;"/>'. format(obj.img))
     image_tag.short_description = 'Image'
+
+
+@admin.register(Theater)
+class TheaterAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['id', 'name']
+
+
+@admin.register(Auditorium)
+class AuditoriumAdmin(admin.ModelAdmin):
+    search_fields = ['name']
+    list_display = ['idx', 'name', 'theater_num']
--- main/models.py
+++ main/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 # Create your models here.
+
+
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     open_movie = models.DateTimeField(null=True)
@@ -16,3 +18,33 @@ class Movie(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Theater(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=250)
+
+    class Meta:
+        db_table = 'theater'
+        verbose_name = '극장',
+        verbose_name_plural = '극장'
+
+    def __str__(self):
+        return self.name
+
+
+class Auditorium(models.Model):
+    theater_num = models.ForeignKey(
+        Theater, null=True, blank=True, on_delete=models.CASCADE)
+    idx = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    seats = models.IntegerField()
+
+    class Meta:
+        db_table = 'auditorium'
+        verbose_name = '상영관',
+        verbose_name_plural = '상영관'
+
+    def __str__(self):
+        return self.name
